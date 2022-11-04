f24bc164ca7a5a9b932c9af76716bba13380ab7a
Nekhlesh1 <Nekhlesh@gmail.com>
2022-11-04 11:30:38 +0530

    initial commit

---
+++ GraphQlProject/Services/MenuService.cs
@@ -0,0 +1,30 @@
+﻿using GraphQlProject.Data;
+using GraphQlProject.Interfaces;
+using GraphQlProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQlProject.Services
+{
+    public class MenuService : IMenu
+    {
+        private GraphQLDbContext _dbcontext;
+        public MenuService(GraphQLDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        public Menu AddMenu(Menu menu)
+        {
+            _dbcontext.Add(menu);
+            _dbcontext.SaveChanges();
+            return menu;
+        }
+
+        public List<Menu> GetMenus()
+        {
+           return _dbcontext.Menus.ToList();
+        }
+    }
+}
